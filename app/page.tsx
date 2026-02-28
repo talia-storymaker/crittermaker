8c2612c2b4c5d64265afa83bfdc6d62e227e076d
Talia Hatfield <talia.j.degisi@gmail.com>
2026-02-27 21:35:51 -0500

    Add ability to download png

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import sty from "./page.module.css";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useState } from "react";
+import { Suspense, useState, useRef } from "react";
 import Critter from "./components/Critter";
 import { OrbitControls } from "@react-three/drei";
+import Link from "next/link";
 
 export default function Home() {
   const [mainColor, setMainColor] = useState(() => {
@@ -30,6 +31,8 @@ export default function Home() {
   });
   const [showSavedStatus, setShowSavedStatus] = useState(false);
 
+  const downloadLink = useRef<HTMLAnchorElement>(null);
+
   function saveCritter(name: string, mainColor: string) {
     try {
       const critters = JSON.parse(localStorage.getItem("critters") || "[]");
@@ -42,30 +45,47 @@ export default function Home() {
     }
   }
 
+  function downloadImg() {
+  const canvas = document.querySelector("canvas");
+  if (canvas) {
+    canvas.toBlob((blob) => {
+      if (blob && downloadLink.current) {
+        downloadLink.current.href = URL.createObjectURL(blob);
+        downloadLink.current.click();
+      } else {
+        console.error("Error downloading image.");
+      }
+    }, "image/png");
+  } else {
+    console.error("No canvas found.");
+  }
+}
+
   return (
     <div className="page">
       <main className="main">
         <h1>
-          <div>3D CritterMaker</div> 
+          <div>3D CritterMaker</div>
           <div className="subtitle">Inspired by Animal Crossing</div>
         </h1>
         <div className={sty.critterContainer}>
-          <Canvas 
+          <Canvas
+            gl={{ preserveDrawingBuffer: true }}
             className={sty.critter}
-            style={{ width: '30rem', height: '35rem' }}
+            style={{ width: "30rem", height: "35rem" }}
             camera={{
               position: [0, 0, 10],
             }}
             shadows
           >
             <ambientLight color={"#fff"} intensity={3} />
-            <spotLight 
-              position={[10, 10, 10]} 
-              angle={0.15} 
-              penumbra={3} 
-              decay={0} 
-              intensity={Math.PI} 
-              castShadow 
+            <spotLight
+              position={[10, 10, 10]}
+              angle={0.15}
+              penumbra={3}
+              decay={0}
+              intensity={Math.PI}
+              castShadow
             />
             <OrbitControls />
             <Suspense fallback={null}>
@@ -94,6 +114,10 @@ export default function Home() {
             <button onClick={() => saveCritter(name, mainColor)}>
               Save{showSavedStatus ? "d!" : ""}
             </button>
+            <button onClick={() => downloadImg()}>Download .png</button>
+            <Link href="" download ref={downloadLink} className="display-none">
+              Invisible download link (script-activated)
+            </Link>
           </form>
         </div>
       </main>
