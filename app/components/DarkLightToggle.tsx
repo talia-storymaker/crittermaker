c7e5d986722009b2c1cc10caca559bfb60ad0209
Talia Hatfield <talia.j.degisi@gmail.com>
2026-04-02 16:00:14 -0400

    Make theme preference saveable

--- app/components/DarkLightToggle.tsx
+++ app/components/DarkLightToggle.tsx
@@ -8,20 +8,37 @@ function DarkLightToggle() {
   useEffect(() => {
     const media = window.matchMedia("(prefers-color-scheme: light)");
     setIsLight(media.matches);
+    if (localStorage.getItem("theme") === "light") {
+      setIsLight(true);
+    } else if (localStorage.getItem("theme") === "dark") {
+      setIsLight(false);
+    }
   }, []);
 
   return (
-    <>
-      Dark Mode{" "}
+    <span className="theme-toggle-container">
+      <span className="theme-toggle">Dark Mode{" "}
       <Switch
         checked={isLight}
-        onChange={(e) => setIsLight(e.target.checked)}
+        onChange={(e) => {
+          setIsLight(e.target.checked);
+          localStorage.setItem("theme", e.target.checked ? "light" : "dark");
+        }}
         slotProps={{
           input: { "aria-label": "Light Mode toggle" },
         }}
       />{" "}
-      Light Mode
-    </>
+      Light Mode</span>
+      <button
+        onClick={() => {
+          localStorage.removeItem("theme");
+          const media = window.matchMedia("(prefers-color-scheme: light)");
+          setIsLight(media.matches);
+        }}
+      >
+        Reset to System Preference
+      </button>
+    </span>
   );
 }
 
